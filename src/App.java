d52ca4dd692c286afe29a45725a39320013e4590
Servet Arslan <arslannservett@gmail.com>
2022-05-12 23:02:02 +0300

    Frame of game was designed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
+    static void howToPlay() {
+        System.out.println("*** WELCOME to MINESWEEPER ***");
+        System.out.println("* Before you start...");
+        System.out.println("- You have to indicate the row and column of the game map.");
+        System.out.println("- The game map's min size can be 2x2 and the max size can be 10x10.");
+        System.out.println("- Let's start the game.");
+        System.out.println("************ START ************");
+    }
+
     public static void main(String[] args) {
+        howToPlay();
+
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.startGame();
+        minesweeper.printInfos();
+
+        minesweeper.createMap();
+        minesweeper.printMap();
+
+
 
     }
 }
--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,3 +1,90 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Minesweeper {
 
+    String[][] map;
+    String[][] userMap;
+    int row;
+    int column;
+    int mine;
+    String mineMark = " ✦ "; //"** ";//"☠ "; //
+    String boxMark = "██ ";
+
+    Minesweeper() {
+
+    }
+
+    int mineRatio() {
+        return (this.row * this.column) / 4;
+    }
+
+    void createMap() {
+        int mineLocation;
+        int mineCount = this.mine;
+        Random randomLocation = new Random();
+
+        for (int i = 0; i < this.map.length; i++) {
+            int newRow = 0;
+            for (int j = 0; j < this.map[i].length; j++) {
+                mineLocation = randomLocation.nextInt(100);
+
+                if (mineCount-- > 0 && mineLocation < 50){
+                    this.map[i][j] = this.mineMark;
+                } else {
+                    this.map[i][j] = this.boxMark;
+                }
+            }
+        }
+
+    }
+
+    void printMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSuitable(int num, int minNum, int maxNum) {
+        return num >= minNum && num <= maxNum;
+    }
+
+    void startGame() {
+        Scanner input = new Scanner(System.in);
+
+        int minRowAndColumn = 2;
+        int maxRowAndColumn = 10;
+
+        while (this.row > maxRowAndColumn || this.row < minRowAndColumn) {
+            System.out.print("Row: ");
+            this.row = input.nextInt();
+            if (!isSuitable(this.row, minRowAndColumn, maxRowAndColumn)) {
+                System.out.println("* The row must be in the range of 2-10.");
+            }
+        }
+        while (this.column > maxRowAndColumn || this.column < minRowAndColumn) {
+            System.out.print("Column: ");
+            this.column = input.nextInt();
+            if (!isSuitable(this.column, minRowAndColumn, maxRowAndColumn)) {
+                System.out.println("* The column must be in the range of 2-10.");
+            }
+        }
+
+        this.map = new String[this.row][this.column];
+        this.userMap = new String[this.row][this.column];
+        this.mine = mineRatio();
+
+    }
+
+    void printInfos() {
+        System.out.println();
+        System.out.println("*** INFOS ***");
+        System.out.println("Mine: " + this.mine);
+        System.out.println("Mine Mark: " + this.mineMark);
+        System.out.println("Box Mark : " + this.boxMark);
+        System.out.println("*************");
+    }
 }
